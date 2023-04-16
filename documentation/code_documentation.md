009f31b5596b351d0ac59ba4d7a7d84b1a7dc086
rekky1aws <killikomfr@gmail.com>
2023-04-16 11:44:21 +0200

    Game table init

---
+++ documentation/code_documentation.md
@@ -0,0 +1,20 @@
+# Code Documentation
+
+## intToChess
+This function is used to display chess pieces instead of numbers to make it easier to read in game.
+
+| Int | Chess piece  | Unicode Character |
+|:----|:-------------|:------------------|
+| 0   | Empty Space  | '  '               |
+| 10  | White King   | 'wK'               |
+| 11  | White Queen  | 'wQ'               |
+| 12  | White Rook   | 'wR'               |
+| 13  | White Bishop | 'wB'               |
+| 14  | White Knight | 'wK'               |
+| 15  | White Pawn   | 'wP'               |
+| 20  | Black King   | 'bK'               |
+| 21  | Black Queen  | 'bQ'               |
+| 22  | Black Rook   | 'bR'               |
+| 23  | Black Bishop | 'bB'               |
+| 24  | Black Kinght | 'bK'               |
+| 25  | Black Pawn   | 'bP'               |
--- main.c
+++ main.c
@@ -2,22 +2,35 @@
 
 #define TABLE_SIZE 8
 
-/*
+// PROTOTYPES
 int initTable (int table[TABLE_SIZE][TABLE_SIZE]);
-int displayTable (int **table);
-char intToChess (int value)
-*/
+int displayTable (int table[TABLE_SIZE][TABLE_SIZE]);
+char *intToChess (int value);
+
 
 // FUNCTIONS
 int initTable (int table[TABLE_SIZE][TABLE_SIZE])
 {
+	int base[TABLE_SIZE][TABLE_SIZE] = {
+		{22,24,23,21,20,23,24,22},
+		{25,25,25,25,25,25,25,25},
+		{00,00,00,00,00,00,00,00},
+		{00,00,00,00,00,00,00,00},
+		{00,00,00,00,00,00,00,00},
+		{00,00,00,00,00,00,00,00},
+		{15,15,15,15,15,15,15,15},
+		{12,14,13,11,10,13,14,15}
+	};
+
 	for (int i = 0; i < TABLE_SIZE; ++i)
 	{
 		for (int j = 0; j < TABLE_SIZE; ++j)
 		{
-			table[i][j] = -1;
+			table[i][j] = base[i][j];
 		}
 	}
+
+	return 0;
 }
 
 int displayTable (int table[TABLE_SIZE][TABLE_SIZE])
@@ -26,7 +39,7 @@ int displayTable (int table[TABLE_SIZE][TABLE_SIZE])
 	printf("|");
 	for (int i = 0; i < TABLE_SIZE; ++i)
 	{
-		printf("---|");
+		printf("----|");
 	}
 	printf("\n");
 
@@ -36,13 +49,13 @@ int displayTable (int table[TABLE_SIZE][TABLE_SIZE])
 		printf("|");
 		for (int j = 0; j < TABLE_SIZE; ++j)
 		{
-			printf(" %d |", table[i][j]);
+			printf(" %s |", intToChess(table[i][j]) );
 		}
 		printf("\n");
 		printf("|");
 		for (int i = 0; i < TABLE_SIZE; ++i)
 		{
-			printf("---|");
+			printf("----|");
 		}
 		printf("\n");
 	}
@@ -52,11 +65,158 @@ int displayTable (int table[TABLE_SIZE][TABLE_SIZE])
 	return 0;
 }
 
-char intToChess (int value)
+/*
+char *intToChess (int value)
 {
 	switch(value) {
-		case -1:
+		// Empty Space
+		case 0 :
+			return " ";
+		break;
 
+	// WHITE
+		// White King
+		case 10 :
+			return "♔";
+		break;
+
+		// White Queen
+		case 11 :
+			return "♕";
+		break;
+		
+		// White Rook
+		case 12 :
+			return "♖";
+		break;
+
+		// White Bishop
+		case 13 :
+			return "♗";
+		break;
+
+		// White Knight
+		case 14 :
+			return "♘";
+		break;
+
+		// White Pawn
+		case 15 :
+			return "♙";
+		break;
+
+	// BLACK
+		// Black King
+		case 20 :
+			return "♚";
+		break;
+
+		// Black Queen
+		case 21 :
+			return "♛";
+		break;
+		
+		// Black Rook
+		case 22 :
+			return "♜";
+		break;
+
+		// Black Bishop
+		case 23 :
+			return "♝";
+		break;
+
+		// Black Knight
+		case 24 :
+			return "♞";
+		break;
+
+		// Black Pawn
+		case 25 :
+			return "♟︎";
+		break;
+		
+		// Error
+		default :
+			return "?";
+		break;
+	}
+}
+*/
+
+char *intToChess (int value)
+{
+	switch(value) {
+		// Empty Space
+		case 0 :
+			return "  ";
+		break;
+
+	// WHITE
+		// White King
+		case 10 :
+			return "wK";
+		break;
+
+		// White Queen
+		case 11 :
+			return "wQ";
+		break;
+		
+		// White Rook
+		case 12 :
+			return "wR";
+		break;
+
+		// White Bishop
+		case 13 :
+			return "wB";
+		break;
+
+		// White Knight
+		case 14 :
+			return "wH";
+		break;
+
+		// White Pawn
+		case 15 :
+			return "wP";
+		break;
+
+	// BLACK
+		// Black King
+		case 20 :
+			return "bK";
+		break;
+
+		// Black Queen
+		case 21 :
+			return "bQ";
+		break;
+		
+		// Black Rook
+		case 22 :
+			return "bR";
+		break;
+
+		// Black Bishop
+		case 23 :
+			return "bB";
+		break;
+
+		// Black Knight
+		case 24 :
+			return "bH";
+		break;
+
+		// Black Pawn
+		case 25 :
+			return "bP";
+		break;
+		
+		// Error
+		default :
+			return "?";
 		break;
 	}
 }
